cb19f3a9fa048d2b35fa64b0b8789d8b3f8cfbb1
benoit modica <modica.b@outlook.fr>
2024-02-10 22:49:52 +0100

    mouseEffect

---
+++ TPmouseEffect/index.js
@@ -0,0 +1,22 @@
+// SELECT LES ITEMS SANS BOUCLE
+// window.addEventListener("mousemove", (e) => {
+//     cursor.style.top = e.y + "px";
+//     cursor.style.left = e.x + "px";
+
+//     mouse1.style.top = e.y + "px";
+//     mouse1.style.left = e.x + "px";
+
+//     mouse2.style.top = e.y + "px";
+//     mouse2.style.left = e.x + "px";
+// })
+
+
+// AVEC LE FOREACH POUR SELECT PLUSIEURS ITEMS
+const mouses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+    mouses.forEach((mouse) => {
+        mouse.style.top = e.y + "px";
+        mouse.style.left = e.x + "px";
+    });
+});
