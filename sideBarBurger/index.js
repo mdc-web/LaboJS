378e0f41bec0142c161c23de934dc0c61a93e6fe
benoit modica <modica.b@outlook.fr>
2024-02-11 21:35:16 +0100

    menuBurger

---
+++ sideBarBurger/index.js
@@ -0,0 +1,19 @@
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+
+// Ranger la sidebar si on click sur le contenu principal
+
+// BONUS
+// Créer en css l'hamburger menu qui se transforme en croix
+
+// Aide
+// Priorités en CSS : id > classe > baliseHtml
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector('.content');
+
+btn.addEventListener('click', () => {
+    sidebar.classList.toggle("active");
+});
+
+content.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+})
